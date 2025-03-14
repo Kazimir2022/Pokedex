8c0775ffd40145f37c93ab5fd75e35abcd9d9e4e
Kazimir Makaranka <m2014ky@gmail.com>
2025-03-14 14:45:56 +0300

    Creating a cache.

--- Sources/Pokedex/PokeAPI/PokeAPI.swift
+++ Sources/Pokedex/PokeAPI/PokeAPI.swift
@@ -9,7 +9,7 @@ import Vapor
 
 extension Request {
   public var pokeAPI: PokeAPI {
-    .init(client: self.client)
+    .init(client: self.client, cache: self.cache)
   }
 }
 
@@ -18,17 +18,39 @@ public final class PokeAPI {
   /// The HTTP client powering this API.
   let client: Client
   
+  /// Cache to check before calling API.
+  let cache: Cache
+  
   /// Creates a new `PokeAPI` wrapper from the supplied client and cache.
-  init(client: Client) {
+  init(client: Client, cache: Cache) {
     self.client = client
+    self.cache = cache
   }
   
   /// Returns `true` if the supplied Pokemon name is real.
   ///
   /// - parameter name: The name to verify.
   public func verify(name: String) -> EventLoopFuture<Bool> {
+    /// Canonicalize input name.
+    let name = name.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+    
+    /// Check cache first.
+    return cache.get(name, as: Bool.self).flatMap { verified in
+      if let verified = verified {
+        return self.client.eventLoop.makeSucceededFuture(verified)
+      } else {
+        return self.uncachedVerify(name: name).flatMap { verified in
+          /// Cache result for next time.
+          return self.cache.set(name, to: verified)
+            .transform(to: verified)
+        }
+      }
+    }
+  }
+
+  private func uncachedVerify(name: String) -> EventLoopFuture<Bool> {
     /// Query the PokeAPI.
-    return fetchPokemon(named: name).flatMapThrowing { res in
+    return fetchPokemon(named: name).flatMapThrowing { res -> Bool in
       switch res.status.code {
       case 200..<300:
         /// The API returned 2xx which means this is a real Pokemon name
@@ -45,7 +67,6 @@ public final class PokeAPI {
   
   /// Fetches a pokemen with the supplied name from the PokeAPI.
   private func fetchPokemon(named name: String) -> EventLoopFuture<ClientResponse> {
-    let name = name.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
     return client.get("https://pokeapi.co/api/v2/pokemon/\(name)")
   }
 }
